79beea5eea0995c981d480d71e31e9a330f7f476
JMFOWLE4904 <bravesfan.awesome@gmail.com>
2017-06-12 11:20:16 -0500

    Add files via upload

---
+++ Hello-World/Hello World.c
@@ -0,0 +1,13 @@
+/* 
+ * Jakob Fowler
+ * Program #1
+ * Hello World
+ * 06/12/17
+*/
+#include "simpletools.h"                     
+
+int main()                                    // Main function
+{
+ print("War Eagle\n");
+ print("Roar Lions");
+}
