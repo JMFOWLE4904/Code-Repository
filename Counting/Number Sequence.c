de34acd1e7f95b458c5047b509cd856f44e06f87
JMFOWLE4904 <bravesfan.awesome@gmail.com>
2017-06-13 09:59:23 -0500

    Initial Commit

---
+++ Counting/Number Sequence.c
@@ -0,0 +1,19 @@
+/*
+ * Jakob Fowler 
+ * Program #3
+ * Counting with a loop
+ * 06/13/17 
+*/
+#include "simpletools.h"                      // Include simple tools
+
+int main()                                    // Main function
+{
+  int x = 33;
+  
+  while (x < 128)
+  {
+   print("%c\n", x);
+   x = x + 1;
+  }
+   print("Done!");
+}
