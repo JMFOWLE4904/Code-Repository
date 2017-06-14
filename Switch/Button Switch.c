a745f39c2f62a1a677ceb6cab5e0dc47db51f7d5
JMFOWLE4904 <bravesfan.awesome@gmail.com>
2017-06-14 10:31:35 -0500

    Additional Commit

--- Switch/Button Switch.c
+++ Switch/Button Switch.c
@@ -7,6 +7,9 @@ int main()
  
   while(1)
   {
+ /* If you use a different circut than I did which was a pressesd low circut 
+    you must change the if statement from asking low to asking high, 0 to 1*/
+   
     if (input(0) == 0)   
     {
      putChar(HOME);
