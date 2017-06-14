b2ab3e999a87c1ff038f002b5d18fdcee8cccee2
JMFOWLE4904 <bravesfan.awesome@gmail.com>
2017-06-14 18:54:49 -0500

    Initial Commit

--- Hello-World/Hello World.c
+++ Hello-World/Hello World.c
@@ -1,9 +1,4 @@
-/* 
- * Jakob Fowler
- * Program #1
- * Hello World
- * 06/12/17
-*/
+
 #include "simpletools.h"                     
 
 int main()                                    // Main function
